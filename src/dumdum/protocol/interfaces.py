8dd156ba14952e66dbeee5cfc0efd162ce4d6c79
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-03-09 11:42:40 -0500

    fix: change Protocol signature to avoid list invariance

--- src/dumdum/protocol/interfaces.py
+++ src/dumdum/protocol/interfaces.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import Sequence
 
 
 class Protocol(ABC):
     @abstractmethod
-    def receive_bytes(self, data: bytes) -> tuple[list[object], bytes]:
+    def receive_bytes(self, data: bytes) -> tuple[Sequence[object], bytes]:
         """Receive bytes from the sender."""
