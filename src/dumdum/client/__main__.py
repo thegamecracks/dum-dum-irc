0c7c7ba3e924ef5ae773ec2014506d30f5e1c8f1
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-03-11 16:37:34 -0400

    feat: add "dumdum appdirs" command

--- src/dumdum/client/__main__.py
+++ src/dumdum/client/__main__.py
@@ -1,4 +1,5 @@
 """Start the client interface for connecting to dumdum servers."""
+
 from __future__ import annotations
 
 import argparse
@@ -25,12 +26,31 @@ def main():
         default=0,
         help="Increase logging verbosity",
     )
+    parser.set_defaults(mode="gui")
+
+    commands = parser.add_subparsers()
+
+    appdirs = commands.add_parser(
+        "appdirs",
+        description="Show data directories used by dumdum.",
+    )
+    appdirs.set_defaults(mode="appdirs")
 
     args = parser.parse_args()
     verbose: int = args.verbose
+    mode: str = args.mode
 
     configure_logging(verbose)
 
+    if mode == "gui":
+        run_gui()
+    elif mode == "appdirs":
+        show_appdirs()
+    else:
+        raise RuntimeError(f"Unknown mode {mode!r}")
+
+
+def run_gui() -> None:
     with EventThread() as event_thread:
         app = TkApp(
             event_thread=event_thread,
@@ -38,6 +58,12 @@ def main():
         )
         app.switch_frame(ConnectFrame(app))
         app.mainloop()
+
+
+def show_appdirs() -> None:
+    from dumdum.appdirs import APP_DIRS
+
+    print("user_data_path =", APP_DIRS.user_data_path)
 
 
 @contextlib.contextmanager
