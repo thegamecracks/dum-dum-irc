59b8bd0678effb6f730a51689eab82f787e2f17a
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-03-11 16:28:57 -0400

    feat: persist and restore last connection fields

--- src/dumdum/client/connect_frame.py
+++ src/dumdum/client/connect_frame.py
@@ -36,15 +36,22 @@ class ConnectFrame(Frame):
         self.connect = Button(self, text="Connect", command=self.do_connect)
         self.connect.grid(row=2, column=1, sticky="e")
 
-        # TODO: replace hardcoded defaults with user data
-        self.host_entry_var.set("127.0.0.1")
-        self.port_entry_var.set("6667")
-        self.nick_entry_var.set("Somebody")
+        self.apply_last_connect_entries()
 
         self._connect_bind = app.bind("<Return>", lambda event: self.do_connect())
 
         self._connection_attempt = None
         self._destroying = False
+
+    def apply_last_connect_entries(self) -> None:
+        with self.app.store_factory() as store:
+            host = store.get_setting("last-connect-host", "127.0.0.1")
+            port = store.get_setting("last-connect-port", 6667)
+            nick = store.get_setting("last-connect-nick", "Somebody")
+
+        self.host_entry_var.set(host)
+        self.port_entry_var.set(port)
+        self.nick_entry_var.set(nick)
 
     def do_connect(self) -> None:
         fut = self._connection_attempt
@@ -55,11 +62,19 @@ class ConnectFrame(Frame):
         port = int(self.port_entry_var.get())
         nick = self.nick_entry_var.get()
 
+        self.set_last_connect_entries(host, port, nick)
+
         coro = self.app.attempt_connection(host, port, nick)
         self._connection_attempt = self.app.submit(coro)
 
         self.connect.state(["disabled"])
         self._connection_attempt.add_done_callback(self._on_connection_attempt)
+
+    def set_last_connect_entries(self, host: str, port: int, nick: str) -> None:
+        with self.app.store_factory() as store:
+            store.set_setting("last-connect-host", host)
+            store.set_setting("last-connect-port", port)
+            store.set_setting("last-connect-nick", nick)
 
     def destroy(self) -> None:
         self._destroying = True
